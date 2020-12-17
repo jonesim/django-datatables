56e325eb6d522478056152eb1e67c2b965e3c17b
Ian Jones <ian@cobi.co.uk>
2020-12-17 09:42:53 +0000

    Add setup files

---
+++ readme.md
@@ -0,0 +1,5 @@
+Django views and templates to use datatables with dynamic javascript filters.
+
+A docker-compose script is included to create a container with a django project with sample data to demonstrate usage. 
+
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-filtered-datatables",
+    version="0.0.1",
+    author="Ian Jones",
+    description="Django views with javascript filters using Datatables",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonesim/django-datatables",
+    packages=['django_datatables'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
