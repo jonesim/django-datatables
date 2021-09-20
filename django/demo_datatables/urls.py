f00bdf2f946068e4d399d19942178b85aabfd5b5
jonesim <ian@cobi.co.uk>
2021-09-20 09:27:56 +0100

    Allow urls to be configured with show_src_code

--- django/demo_datatables/urls.py
+++ django/demo_datatables/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.apps import apps
 
 urlpatterns = [
     path('', include('show_src_code.urls')),
     path('admin/', admin.site.urls),
 ]
-
-a = apps.get_app_configs()
-for c in a:
-    if hasattr(c, 'urls'):
-        urlpatterns += [path('', include(c.urls))]
