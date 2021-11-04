0f5a1bbd2f653d0c2038929053b8529740970539
jonesim <ian@cobi.co.uk>
2021-11-04 14:17:24 +0000

    Add urls to examples

--- django/demo_datatables/urls.py
+++ django/demo_datatables/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include('show_src_code.urls')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/datatable_examples/favicon.ico', permanent=True)),
+    path('datatables', include('django_datatables.urls'))
 ]
