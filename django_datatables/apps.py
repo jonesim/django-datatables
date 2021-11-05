bbd5a47ab7f334281f78e3e0ec2cf4e4e55dfb35
Tom Turner <tom@twt.me.uk>
2021-11-05 14:39:39 +0000

    Added verbose_name to the app

--- django_datatables/apps.py
+++ django_datatables/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DjangoDatatablesConfig(AppConfig):
     name = 'django_datatables'
     default_auto_field = 'django.db.models.AutoField'
+    verbose_name = 'Django datatables'
