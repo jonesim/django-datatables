623ca4129a6b8706d95a1af4a902061fd1aa34ca
ian_jones <ian@cobi.co.uk>
2025-07-25 13:45:34 +0100

    Rename datatables.py

--- django_datatables/datatables/__init__.py
+++ django_datatables/datatables/__init__.py
@@ -1,4 +1,4 @@
 from .column_initialiser import ColumnInitialisor
-from .datatables import DatatableExcludedRow,  DatatableTable, HorizontalTable
+from .datatable_table import DatatableExcludedRow, DatatableTable, HorizontalTable
 from .datatable_error import DatatableError
 from .views import DatatableView
